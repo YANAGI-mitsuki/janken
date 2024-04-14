af57d20ac896336420938f692de412f39a8a1e54
Mitsuki Yanagi <iganaymjs2@gmail.com>
2024-04-14 10:40:25 +0900

    feat:トップページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'game/start'
   get 'game/result'
   get 'top/index'
+  root to: 'top#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
