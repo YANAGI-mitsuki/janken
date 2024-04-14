fd976d8fff5ebc2ae27188aa40cbca2913955925
Mitsuki Yanagi <iganaymjs2@gmail.com>
2024-04-14 10:09:32 +0900

    feat:アプリケーションの新規作成とTopコントローラとGameコントローラの作成

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'game/start'
+  get 'game/result'
+  get 'top/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
